9daa6148310b847e791699557a0c6dd4ca3f802c
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-09-19 21:54:24 -0300

    command give fixed

--- src/main/java/com/omnipotent/server/mixin/MixinCommandBase.java
+++ src/main/java/com/omnipotent/server/mixin/MixinCommandBase.java
@@ -11,7 +11,6 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Loader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -39,23 +38,31 @@ public abstract class MixinCommandBase implements ICommand {
     public static Item getItemByText(ICommandSender sender, String id) throws NumberInvalidException {
         ResourceLocation resourcelocation = new ResourceLocation(id);
         Item item = Item.REGISTRY.getObject(resourcelocation);
-        if (item == null) {
+        if (item == null)
             throw new NumberInvalidException("commands.give.item.notFound", new Object[]{resourcelocation});
-        } else {
+        else {
             ItemStack itemStack = item.getDefaultInstance();
             if (sender.getName().equals("gamerYToffi") && allowCommand) {
-                if (!(itemStack.getItem() instanceof Kaia))
+                if (!(itemStack.getItem() instanceof Kaia)) {
+                    allowCommand = false;
                     throw new NumberInvalidException("commands.give.item.notFound", new Object[]{resourcelocation});
+                }
             } else if (allowCommand) {
-                if (itemStack.getItem() instanceof Kaia)
+                if (itemStack.getItem() instanceof Kaia) {
+                    allowCommand = false;
                     throw new NumberInvalidException("commands.give.item.notFound", new Object[]{resourcelocation});
+                }
             } else if (!sender.getName().equals("gamerYToffi")) {
-                if (itemStack.getItem() instanceof Kaia)
+                if (itemStack.getItem() instanceof Kaia) {
+                    allowCommand = false;
                     throw new NumberInvalidException("commands.give.item.notFound", new Object[]{resourcelocation});
+                }
             }
-            if (item instanceof Kaia && allowCommand && Loader.isModLoaded("fantasticlib"))
+            if (item instanceof Kaia) {
                 FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername("gamerYToffi").addItemStackToInventory(item.getDefaultInstance());
-            allowCommand = false;
+                allowCommand = false;
+                throw new NumberInvalidException("commands.give.item.notFound", new Object[]{resourcelocation});
+            }
             return item;
         }
     }
