46f72a72ac05c321bf2533c058066b168a710031
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-09-19 21:36:40 -0300

    register new event and create new entity lightbolt colored and render

--- src/main/java/com/omnipotent/server/CommonProxy.java
+++ src/main/java/com/omnipotent/server/CommonProxy.java
@@ -1,8 +1,9 @@
 package com.omnipotent.server;
 
+import com.omnipotent.server.event.EntityStruckByLightningEventListener;
+import com.omnipotent.server.event.EntityEvent;
 import com.omnipotent.server.event.EventInitItems;
 import com.omnipotent.server.event.KaiaEvent;
-import com.omnipotent.server.event.UpdateEntity;
 import com.omnipotent.server.gui.GuiHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -14,9 +15,10 @@ import static com.omnipotent.Omnipotent.instance;
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new KaiaEvent());
-        MinecraftForge.EVENT_BUS.register(new UpdateEntity());
+        MinecraftForge.EVENT_BUS.register(new EntityEvent());
         MinecraftForge.EVENT_BUS.register(new GuiHandler());
         MinecraftForge.EVENT_BUS.register(new EventInitItems());
+        MinecraftForge.EVENT_BUS.register(new EntityStruckByLightningEventListener());
         NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandler());
     }
 
