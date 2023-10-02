aba34d3b3b5ff8383b0a775f8d12698478db0590
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-10-02 19:37:28 -0300

    news constants

--- src/main/java/com/omnipotent/util/KaiaConstantsNbt.java
+++ src/main/java/com/omnipotent/util/KaiaConstantsNbt.java
@@ -1,33 +1,50 @@
 package com.omnipotent.util;
 
 public class KaiaConstantsNbt {
-    public static final String blockBreakArea = "areaBloco";
+    //bolean values
     public static final String counterAttack = "counterAttack";
     public static final String killAllEntities = "killAllEntities";
-    public static final String rangeAttack = "rangeAttack";
     public static final String killFriendEntities = "killFriendEntities";
-    public static final String ownerName = "ownerName";
-    public static final String ownerID = "ownerID";
-    public static final String playersCantRespawn = "playerscantresSpawn";
     public static final String attackYourWolf = "attackYourWolf";
     public static final String interactLiquid = "interactLiquid";
     public static final String noBreakTileEntity = "noBreakTileEntity";
-    public static final String kaiaEnchant = "enchantKaia";
-    public static final String kaiaPotion = "enchantPotion";
-    public static final String kaiaDimension = "enchantdimension";
-    public static final String maxCountSlot = "maxcountslot";
-    public static final String autoBackPack = "autobackpack";
     public static final String autoBackPackEntities = "autobackpackentities";
+    public static final String chargeItemsInInventory = "chargeitemsininventory";
+    public static final String summonLightBoltsInKill = "summonlightboltsinkill";
+    public static final String autoBackPack = "autobackpack";
+    public static final String playersCantRespawn = "playerscantresSpawn";
+    public static final String banEntitiesAttacked = "banEntitiesAttacked";
+    public static final String autoKill = "autoKill";
+
+
+    //number values
+    public static final String blockBreakArea = "areaBloco";
+    public static final String rangeAttack = "rangeAttack";
+    public static final String maxCountSlot = "maxcountslot";
     public static final String blockReachDistance = "blockReachDistance";
+    public static final String rangeAutoKill = "rangeautokill";
+
+
+    //String values
+    public static final String ownerName = "ownerName";
+    public static final String ownerID = "ownerID";
+
+    //list values
+
     public static final String playersDontKill = "playersdontkill";
     public static final String playersWhoShouldNotKilledInCounterAttack = "playerdontkillcounter";
     public static final String playerDontKillInDirectAttack = "playerdontkillindirectAttack";
     public static final String entitiesCantKill = "entitiescantkill";
-    public static final String rangeAutoKill = "rangeautokill";
+
+
+    public static final String effectsBlockeds = "effectsBlocked";
+
+    //other values
+    public static final String kaiaEnchant = "enchantKaia";
     public static final String kaiaBlockSpectator = "kaiablockspectator";
     public static final String kaiaBlockCreative = "kaiablockcreative";
-    public static final String chargeItemsInInventory = "chargeitemsininventory";
-    public static final String summonLightBoltsInKill = "summonlightboltsinkill";
+    public static final String kaiaDimension = "enchantdimension";
 
+    public static final String kaiaPotion = "enchantPotion";
 
 }
