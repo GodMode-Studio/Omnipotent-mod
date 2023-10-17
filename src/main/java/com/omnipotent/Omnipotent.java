03b2320018cdc3f5a0b61acebdfc1597050ba595
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-10-17 19:05:17 -0300

    new version alpha 8.4

--- src/main/java/com/omnipotent/Omnipotent.java
+++ src/main/java/com/omnipotent/Omnipotent.java
@@ -33,7 +33,7 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 public class Omnipotent {
     public static final String MODID = "omnipotent";
     public static final String NAME = "Omnipotent Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "alpha 8.4";
     public static final OmnipotentTab omnipotentTab = new OmnipotentTab("Omnipotent mod");
     public static SimpleNetworkWrapper channel = NetworkRegistry.INSTANCE.newSimpleChannel("omnipotent");
     public static final ResourceLocation KAIACAP = new ResourceLocation(MODID, "kaiabrand");
