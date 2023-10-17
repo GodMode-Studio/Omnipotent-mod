284dd51c1c50d58ce9cbfbf6cf3197ad2e47fc96
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-10-17 19:01:25 -0300

    class remodeled

--- src/main/java/com/omnipotent/client/gui/elementsmod/GuiButtonMod.java
+++ src/main/java/com/omnipotent/client/gui/elementsmod/GuiButtonMod.java
@@ -1,9 +1,31 @@
 package com.omnipotent.client.gui.elementsmod;
 
+import com.omnipotent.constant.NbtBooleanValues;
+import com.omnipotent.constant.NbtNumberValues;
 import net.minecraft.client.gui.GuiButton;
 
 public class GuiButtonMod extends GuiButton {
     public Runnable runnable;
+    private NbtBooleanValues valueNbt;
+
+    public NbtNumberValues getValueNbtNumber() {
+        return valueNbtNumber;
+    }
+
+    public void setValueNbtNumber(NbtNumberValues valueNbtNumber) {
+        this.valueNbtNumber = valueNbtNumber;
+    }
+
+    private NbtNumberValues valueNbtNumber;
+
+
+    public NbtBooleanValues getValueNbt() {
+        return valueNbt;
+    }
+
+    public void setValueNbt(NbtBooleanValues valueNbt) {
+        this.valueNbt = valueNbt;
+    }
 
     public GuiButtonMod(int buttonId, int x, int y, String buttonText) {
         super(buttonId, x, y, buttonText);
