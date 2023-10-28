7610df45d92f2765f353438303dc8b0e6f86260f
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-10-28 13:34:45 -0300

    fixed bugs packets inicialible in commonproxy

--- src/main/java/com/omnipotent/client/ClientProxy.java
+++ src/main/java/com/omnipotent/client/ClientProxy.java
@@ -2,11 +2,15 @@ package com.omnipotent.client;
 
 import com.omnipotent.client.event.EventClient;
 import com.omnipotent.client.event.EventInitTextures;
+import com.omnipotent.client.event.EventPlayerNameFormat;
 import com.omnipotent.client.event.KaiaToolTip;
 import com.omnipotent.client.key.KeyEvent;
 import com.omnipotent.client.key.KeyInit;
+import com.omnipotent.client.render.RenderCustomLightningBolt;
 import com.omnipotent.server.CommonProxy;
+import com.omnipotent.server.entity.CustomLightningBolt;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -18,6 +22,8 @@ public class ClientProxy extends CommonProxy {
         MinecraftForge.EVENT_BUS.register(new KeyEvent());
         MinecraftForge.EVENT_BUS.register(new EventInitTextures());
         MinecraftForge.EVENT_BUS.register(new EventClient());
+        MinecraftForge.EVENT_BUS.register(new EventPlayerNameFormat());
+        RenderingRegistry.registerEntityRenderingHandler(CustomLightningBolt.class, RenderCustomLightningBolt::new);
     }
 
     @Override
--- src/main/java/com/omnipotent/server/CommonProxy.java
+++ src/main/java/com/omnipotent/server/CommonProxy.java
@@ -2,6 +2,8 @@ package com.omnipotent.server;
 
 import com.omnipotent.server.event.*;
 import com.omnipotent.server.gui.GuiHandler;
+import com.omnipotent.server.network.NetworkRegister;
+import com.omnipotent.server.network.PacketInicialization;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -11,6 +13,7 @@ import static com.omnipotent.Omnipotent.instance;
 
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
+        NetworkRegister.ACESS.sendToServer(new PacketInicialization());
         MinecraftForge.EVENT_BUS.register(new KaiaEvent());
         MinecraftForge.EVENT_BUS.register(new EntityEvent());
         MinecraftForge.EVENT_BUS.register(new GuiHandler());
