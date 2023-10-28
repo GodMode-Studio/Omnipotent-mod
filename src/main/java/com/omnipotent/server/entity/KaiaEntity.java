cc1aa49d99ba67bde5155906833e0833617df74e
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-10-28 13:43:45 -0300

    removed interface IForgeRegistryEntry

--- src/main/java/com/omnipotent/server/entity/KaiaEntity.java
+++ src/main/java/com/omnipotent/server/entity/KaiaEntity.java
@@ -1,14 +1,15 @@
 package com.omnipotent.server.entity;
 
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-import net.minecraftforge.registries.IForgeRegistryEntry;
 
-import javax.annotation.Nullable;
+public class KaiaEntity extends EntityItem {
 
-public class KaiaEntity extends EntityItem implements IForgeRegistryEntry {
+    public boolean kaiaDoNotTrue = false;
+    public EntityPlayer playerInvoker = null;
+
     public KaiaEntity(World worldIn, double x, double y, double z) {
         super(worldIn, x, y, z);
     }
@@ -19,21 +20,5 @@ public class KaiaEntity extends EntityItem implements IForgeRegistryEntry {
 
     public KaiaEntity(World worldIn) {
         super(worldIn);
-    }
-
-    @Override
-    public Object setRegistryName(ResourceLocation name) {
-        return new ResourceLocation("kaia");
-    }
-
-    @Nullable
-    @Override
-    public ResourceLocation getRegistryName() {
-        return new ResourceLocation("kaia");
-    }
-
-    @Override
-    public Class getRegistryType() {
-        return null;
     }
 }
