2a411f843a5ed9ed7a0ff5b0ef542500d26f95a3
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-11-15 22:13:38 -0300

    in development

---
+++ src/main/java/com/omnipotent/server/event/onPlayerRightClickBlockEventListener.java
@@ -0,0 +1,16 @@
+//package com.omnipotent.server.event;
+//
+//import net.minecraft.block.Block;
+//import net.minecraft.world.World;
+//import net.minecraft.world.WorldServer;
+//import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+//import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+//
+//public class onPlayerRightClickBlockEventListener {
+//    @SubscribeEvent
+//    public void onPlayerRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+////        List<ItemStack> collect = event.getContainer().getInventory().stream().filter(item -> item.getItem() instanceof Kaia).collect(Collectors.toList());
+////        event.getContainer().get
+////        Block
+//    }
+//}
