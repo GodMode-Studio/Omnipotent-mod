3fe933ba41f5eba5fd81adb35a5d44f34b4c6f43
gamerYToff <miguellopesdelmondes@gmail.com>
2024-09-24 14:35:47 -0300

    new setters acess

--- src/main/java/com/omnipotent/acessor/IEntityLivingBaseAcessor.java
+++ src/main/java/com/omnipotent/acessor/IEntityLivingBaseAcessor.java
@@ -1,9 +1,15 @@
 package com.omnipotent.acessor;
 
+import net.minecraft.util.DamageSource;
+
 public interface IEntityLivingBaseAcessor {
 
     public int getRecentlyHit();
 
     public void setRecentlyHit(int recentlyHit);
+
+    void setlastDamageSource(DamageSource lastDamageSource);
+
+    void setlastDamageStamp(long lastDamageStamp);
 }
 
