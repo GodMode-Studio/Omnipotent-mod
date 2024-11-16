6c294c535ecb22a043709793bfa906786795d398
gamerYToff <miguellopesdelmondes@gmail.com>
2024-11-16 17:45:28 -0300

    new version 1.3.4

--- src/main/java/com/omnipotent/Omnipotent.java
+++ src/main/java/com/omnipotent/Omnipotent.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
 public class Omnipotent {
     public static final String MODID = "omnipotent";
     public static final String NAME = "Omnipotent Mod";
-    public static final String VERSION = "1.3.3-beta.1";
+    public static final String VERSION = "1.3.4-beta.1";
     public static final OmnipotentTab omnipotentTab = new OmnipotentTab("Omnipotent mod");
     public static final ResourceLocation KAIACAP = new ResourceLocation(MODID, "kaiabrand");
     public static final ResourceLocation BLOCK_MODES_OF_PLAYER = new ResourceLocation(MODID, "blockmodesplayer");
