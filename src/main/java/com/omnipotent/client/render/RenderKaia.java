ad2f819e002fe616b7bc772090c5663d55fa7346
gamerYToff <miguellopesdelmondes@gmail.com>
2024-12-09 01:58:33 -0300

    fixed render of kaia entity

--- src/main/java/com/omnipotent/client/render/RenderKaia.java
+++ src/main/java/com/omnipotent/client/render/RenderKaia.java
@@ -4,18 +4,14 @@ import com.omnipotent.common.entity.KaiaEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.block.model.IBakedModel;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -46,7 +42,7 @@ public class RenderKaia extends Render<KaiaEntity> {
         GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
         GlStateManager.pushMatrix();
         IBakedModel ibakedmodel = this.itemRenderer.getItemModelWithOverrides(itemstack, entity.world, null);
-        this.transform(entity, x, y, z, partialTicks, ibakedmodel);
+        this.transform(entity, x, y, z);
 
         if (this.renderOutlines) {
             GlStateManager.enableColorMaterial();
@@ -74,52 +70,27 @@ public class RenderKaia extends Render<KaiaEntity> {
         super.doRender(entity, x, y, z, entityYaw, partialTicks);
     }
 
-    private void transform(KaiaEntity entity, double x, double y, double z, float partialTicks, IBakedModel kaiaModel) {
-        float f1 = MathHelper.sin(((float) 6000 + partialTicks) / 10.0F + (float) (1 * Math.PI * 2.0D)) * 0.1F + 0.1F;
-        float f2 = kaiaModel.getItemCameraTransforms().getTransform(ItemCameraTransforms.TransformType.GROUND).scale.y;
-        GlStateManager.translate((float) x, (float) y + f1 + 0.25F * f2, (float) z);
-        if (entity.isAttackMode()) {
-            rotationEntityToTarget(entity);
-        }
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-    }
-
-    private void rotationEntityToTarget(KaiaEntity entity) {
+    private void transform(KaiaEntity entity, double x, double y, double z) {
+        GlStateManager.translate(x, y, z);
         EntityLivingBase attackTarget = entity.getAttackTarget();
         if (attackTarget == null) return;
-        final float deltaX = (float) (entity.posX - attackTarget.posX);
-        GlStateManager.rotate(deltaX > 0 ? 90 : 270, 0.0F, 0.0F, 1.0F);
-        final float deltaZ = (float) (entity.posZ - attackTarget.posZ);
-        GlStateManager.rotate(deltaZ > 0 && deltaZ < 1 ? 0 : deltaZ > 0 ? 270 : 90, 1.0F, 0.0F, 0.0F);
+        if(!attackTarget.isEntityAlive())
+            entity.setAttackTarget(null);
+        double dx = attackTarget.posX - entity.posX;
+        double dy = attackTarget.posY - entity.posY;
+        double dz = attackTarget.posZ - entity.posZ;
+        float length = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+        dx /= length;
+        dy /= length;
+        dz /= length;
+        double axisX = dz;
+        double axisY = 0;
+        double axisZ = -dx;
+        double dot = dy;
+        float angle = (float) Math.acos(dot);
+        angle = (float) Math.toDegrees(angle);
+        GlStateManager.rotate(angle, (float) axisX, (float) axisY, (float) axisZ);
     }
-
-//    private String[] readChat() {
-//        Minecraft mc = Minecraft.getMinecraft();
-//        try {
-//            List<String> sentMessages = mc.ingameGUI.getChatGUI().getSentMessages();
-//            String s = sentMessages.get(sentMessages.size() - 1);
-//            String[] words = s.split("=");
-//            if (words.length != 2)
-//                return null;
-//            Double.valueOf(words[1]);
-//            return words;
-//        } catch (Exception e) {
-//        }
-//        if (arr != null) {
-//            String s = arr[0];
-//            float v = 1.0F;
-//            float v1 = 0.0F;
-//            float x1 = s.equalsIgnoreCase("x") ? v : v1;
-//            float y1 = s.equalsIgnoreCase("y") ? v : v1;
-//            float z1 = s.equalsIgnoreCase("z") ? v : v1;
-//            float angle = Float.parseFloat(arr[1]);
-//            if (x1 == v1 || y1 == v1 || z1 == v1)
-//                GlStateManager.rotate(angle, x1,
-//                        y1,
-//                        z1);
-//        }
-//        return null;
-//    }
 
     /**
      * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
